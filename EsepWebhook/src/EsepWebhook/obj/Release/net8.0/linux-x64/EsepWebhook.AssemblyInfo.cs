38cc4a07682a7da6d59e9a43dcfdd0ab2c521acf
KyleHoNguyen <kyle.honguyen@gmail.com>
2024-11-11 19:46:19 -0500

    more changes?

--- EsepWebhook/src/EsepWebhook/obj/Release/net8.0/linux-x64/EsepWebhook.AssemblyInfo.cs
+++ EsepWebhook/src/EsepWebhook/obj/Release/net8.0/linux-x64/EsepWebhook.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EsepWebhook")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1dfce73197e5b87b2094cfefacd7d3846938b6f3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dd7516f7be9259d8cffa9ac5ea5c550e98c35148")]
 [assembly: System.Reflection.AssemblyProductAttribute("EsepWebhook")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EsepWebhook")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
